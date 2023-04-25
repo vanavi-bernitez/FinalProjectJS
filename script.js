11c884d89fa7c2d7b0a4132368c2dfc135d71bf0
Viviana Benitez <viviana.bernitez@gmail.com>
2023-04-25 00:07:52 -0500

    Places search box added, delimited search type to cities

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const initMap = () => {
+
     const startMapOptions = {
         zoom: 13,
         center: {lat: 4.5350, lng: -75.6757}
     } 
-
     const mapElement = document.querySelector('#map');
 
+    // Map creation
     const map = new google.maps.Map(mapElement, startMapOptions);
 
+    // Marker function
     const addMapMarker = (properties) => {
         const mapMarker = new google.maps.Marker({
             position: properties.coords,
-            map
+            map,            
         })
         return mapMarker;
     }
 
-    const startMarket = addMapMarker({coords:{lat: 4.5350, lng: -75.6757}});
+    // Start value and message over the start market
+    const startMarker = addMapMarker({coords:{lat: 4.5350, lng: -75.6757}});
+    const startInfoWindow = new google.maps.InfoWindow({
+        content: '<h2 id="infoWindow"> Hi. Welcome to Armenia! </h2>'
+    })
+    startMarker.addListener('click', () => {
+        startInfoWindow.open(map, startMarker)
+    })
 
+    // When click on the map. Creates a marker
     google.maps.event.addListener(map, 'click', (event) => {
         addMapMarker({coords: event.latLng});
         const latitude = event.latLng.lat();
@@ -26,17 +36,111 @@ const initMap = () => {
         // return latitude, longitude;
     })
 
-    const startInfoWindow = new google.maps.InfoWindow({
-        content: '<h2 id="infoWindow"> Hi. Welcome to Armenia! </h2>'
+    // Create search box
+    const startSearchBoxOptions = {
+        types: ['cities']
+    }
+    const inputLocationElement = document.querySelector('#searchBox');
+    const searchBox = new google.maps.places.SearchBox(inputLocationElement, startSearchBoxOptions); //This makes the magic
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputLocationElement);
+    map.addListener('bounds_changed', () => {
+        searchBox.setBounds(map.getBounds());
     })
 
-    startMarket.addListener('click', () => {
-        startInfoWindow.open(map, startMarket)
-    })
+
+
+
+
+
+    let markers = [];
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+  
+      if (places.length == 0) {
+        return;
+      }
+  
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+  
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+      
+  
+      places.forEach((place) => {
+        if (!place.geometry || !place.geometry.location) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+  
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+
+        
+        console.log(place.geometry.location.lat(), place.geometry.location.lng());
+    
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+          console.log(bounds.extend(place.geometry.location));
+        }
+      });
+      
+      map.fitBounds(bounds);
+    });
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return map
 }
 
-initMap();
+window.initMap = initMap;
+// initMap();
 
 
