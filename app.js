86554adcae0cef024dc84b0324ce660cd941f671
Viviana Benitez <viviana.bernitez@gmail.com>
2023-05-03 16:12:46 -0500

    creating and mounting multiple routes

--- app.js
+++ app.js
@@ -2,60 +2,78 @@ import express from 'express';
 import morgan from 'morgan';
 
 const app = express();
-app.use(morgan('dev'));
+
+// Middlewares
+app.use(morgan('dev')); 
 app.use(express.json());
 
 
-// Callback functions. Handlers
+// Callback functions -> Handlers
 const getAllUsers = (request, response) => {
     response.status(500).json({
         status: 'error',
-        message: 'Route not yet defined'
-    })
+        message: 'Route is not yet defined for getAllUsers'
+    });
 }
 
 const createUser = (request, response) => {
     response.status(500).json({
         status: 'error',
-        message: 'Route not yet defined'
+        message: 'Route not yet defined for createUser'
     })
 }
 
 const getUser = (request, response) => {
     response.status(500).json({
         status: 'error',
-        message: 'Route not yet defined'
+        message: 'Route not yet defined for getUser'
     })
 }
 
 const updateUser = (request, response) => {
     response.status(500).json({
         status: 'error',
-        message: 'Route not yet defined'
+        message: 'Route not yet defined for updateUser'
     })
 }
 
 const deleteUser = (request, response) => {
     response.status(500).json({
         status: 'error',
-        message: 'Route not yet defined'
+        message: 'Route not yet defined for deleteUser'
     })
 }
 
 
-app
-    .route('api/v1/users')
+// Routes --> like mini apps
+const userRouter = express.Router();
+
+userRouter
+    .route('/')
     .get(getAllUsers)
     .post(createUser);
 
-
-app
-    .route('api/v1/users/:id')
+userRouter
+    .route('/:id')
     .get(getUser)
     .patch(updateUser)
-    .delete(deleteUser)
+    .delete(deleteUser);
 
 
+app.use('/api/v1/users', userRouter);
+
+
+
+// app
+//     .route('/api/v1/users')
+//     .get(getAllUsers)
+//     .post(createUser);
+
+// app
+//     .route('/api/v1/users/:id')
+//     .get(getUser)
+//     .patch(updateUser)
+//     .delete(deleteUser);
 
 // app.get('/api/v1/tours', (request, response) => {
 //     response
