1b18536f2e842d5240b5b23dc2163012d4e6beb4
Viviana Benitez <viviana.bernitez@gmail.com>
2023-05-05 11:59:38 -0500

    data base connected

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-import express from 'express';
-import morgan from 'morgan';
-import { userRouter } from './routes/userRouter.js';
-import dotenv from 'dotenv' //the video lies :( i yes need to declare this in app.js
-dotenv.config({path: './config.env'})
+import express from "express";
+import morgan from "morgan";
+import { userRouter } from "./routes/userRouter.js";
+import dotenv from "dotenv"; //the video lies :( i yes need to declare this in app.js
+dotenv.config({ path: "./config.env" });
 
 const app = express();
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 // Middlewares
-if (process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
- 
+
 app.use(express.json());
 // here handle static files
 
 // Mount route --> works like declaration of middleware
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/users", userRouter);
 
-
-export { app }
+export { app };
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-import dotenv from 'dotenv'
-dotenv.config({path: './config.env'})
-import { app } from './app.js'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
+import { app } from "./app.js";
+
+const dataBase = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(dataBase, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then((myConnection) => {
+    console.log(myConnection.connections);
+    console.log("DB connection successful");
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Server running on port: ', port);
+  console.log("Server running on port: ", port);
 });
